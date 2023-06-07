6c16dde646187a7e4b6244531a9012f95d6b64c8
Nahyuntak <hyuntak@snu.ac.kr>
2023-06-07 12:19:23 +0900

    commit

---
+++ Members_DML.sql/Members_Deletes.sql
@@ -0,0 +1,15 @@
+-- SELECT column1, column2, ...
+-- FROM table_name
+-- WHERE condition;
+-- ORDER BY column1, column2, ... ASC|DESC;
+
+DELETE FROM members_hobby ;
+DELETE FROM membewrs_login ;
+DELETE FROM hobby_code ;
+DELETE FROM members ;
+DELETE FROM address_code ;
+DELETE FROM gender_code ;
+
+-- SELECT *
+-- FROM members
+-- WHERE TELEPHONE LIKE '%'||'0-'||'%';
